e0fe45c9ee4f99f28ccf537201e257ab2feaf2a9
Joe Ferguson <joe@Joeferguson.me>
2024-05-20 14:33:22 -0500

    Update link command for either sed
    h/t https://code.vandalsweb.com/vandalsweb/builder-container-debian-stable/-/merge_requests/24/diffs

--- bin/link-to-bento.sh
+++ bin/link-to-bento.sh
@@ -4,22 +4,7 @@
 /bin/ln -f scripts/arm.sh ../bento/packer_templates/scripts/ubuntu/homestead_arm.sh
 
 echo " " > ../bento/packer_templates/scripts/_common/motd.sh
-sed -i 's/${var.os_name}\/cleanup_${var.os_name}.sh/ubuntu\/homestead_amd64.sh/' ../bento/packer_templates/pkr-builder.pkr.hcl
+
+sed -i.bak 's/${var.os_name}\/cleanup_${var.os_name}.sh/ubuntu\/homestead_amd64.sh/' ../bento/packer_templates/pkr-builder.pkr.hcl; rm -f ../bento/packer_templates/*.bak
 # Set disk_size
-sed -i 's/65536/524288/' ../bento/packer_templates/pkr-variables.pkr.hcl
-
-
-
-#sed -i 's/scripts\/cleanup.sh/scripts\/homestead.sh/' ../packer_templates/pkr-builder.pkr.hcl
-#sed -i 's/"cpus": "1"/"cpus": "2"/' ../bento/packer_templates/ubuntu/ubuntu-20.04-amd64.json
-#sed -i 's/"boot_wait": "5s"/"boot_wait": "3s"/' ../bento/packer_templates/ubuntu/ubuntu-20.04-amd64.json
-#sed -i 's/"memory": "1024"/"memory": "2048"/' ../bento/packer_templates/ubuntu/ubuntu-20.04-amd64.json
-#sed -i 's/"disk_size": "65536"/"disk_size": "524288"/' ../bento/packer_templates/ubuntu/ubuntu-20.04-amd64.json
-#sed -i '/\/_common\/motd.sh/d' ../bento/packer_templates/ubuntu/ubuntu-20.04-amd64.json
-
-## Run for ARM
-#sed -i 's/scripts\/cleanup.sh/scripts\/homestead-arm.sh/' ../bento/packer_templates/ubuntu/ubuntu-20.04-arm64.json
-#sed -i 's/"boot_wait": "5s"/"boot_wait": "3s"/' ../bento/packer_templates/ubuntu/ubuntu-20.04-arm64.json
-#sed -i 's/"memory": "1024"/"memory": "2048"/' ../bento/packer_templates/ubuntu/ubuntu-20.04-arm64.json
-#sed -i 's/"disk_size": "65536"/"disk_size": "524288"/' ../bento/packer_templates/ubuntu/ubuntu-20.04-arm64.json
-#sed -i '/\/_common\/motd.sh/d' ../bento/packer_templates/ubuntu/ubuntu-20.04-arm64.json
+sed -i.bak 's/65536/524288/' ../bento/packer_templates/pkr-variables.pkr.hcl; rm -f ../bento/packer_templates/*.bak
