0abf684a09c8157f4fa78fa390f1bba70a82df0d
Joe Ferguson <joe@joeferguson.me>
2024-09-06 10:08:56 -0500

    Add x86 8.4

--- scripts/amd64.sh
+++ scripts/amd64.sh
@@ -473,6 +473,51 @@ else
 
   touch /home/vagrant/.homestead-features/php82
 
+  # PHP 8.4
+  apt-get install -y --allow-change-held-packages \
+  php8.4 php8.4-bcmath php8.4-bz2 php8.4-cgi php8.4-cli php8.4-common php8.4-curl php8.4-dba php8.4-dev \
+  php8.4-enchant php8.4-fpm php8.4-gd php8.4-gmp  php8.4-interbase php8.4-intl php8.4-ldap \
+  php8.4-mbstring php8.4-mysql php8.4-odbc php8.4-opcache php8.4-pgsql php8.4-phpdbg php8.4-readline \
+  php8.4-snmp php8.4-soap php8.4-sqlite3 php8.4-sybase php8.4-tidy php8.4-xml php8.4-xsl \
+  php8.4-zip
+  # not ready yet:
+  # php8.4-imap  php8.4-pspell php8.4-xdebug php8.4-imagick php8.4-memcached php8.4-redis php8.4-xmlrpc
+  # Configure php.ini for CLI
+  sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php/8.4/cli/php.ini
+  sed -i "s/display_errors = .*/display_errors = On/" /etc/php/8.4/cli/php.ini
+  sed -i "s/memory_limit = .*/memory_limit = 512M/" /etc/php/8.4/cli/php.ini
+  sed -i "s/;date.timezone.*/date.timezone = UTC/" /etc/php/8.4/cli/php.ini
+
+  # Configure Xdebug
+  echo "xdebug.mode = debug" >> /etc/php/8.4/mods-available/xdebug.ini
+  echo "xdebug.discover_client_host = true" >> /etc/php/8.4/mods-available/xdebug.ini
+  echo "xdebug.client_port = 9003" >> /etc/php/8.4/mods-available/xdebug.ini
+  echo "xdebug.max_nesting_level = 512" >> /etc/php/8.4/mods-available/xdebug.ini
+  echo "opcache.revalidate_freq = 0" >> /etc/php/8.4/mods-available/opcache.ini
+
+  # Configure php.ini for FPM
+  sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php/8.4/fpm/php.ini
+  sed -i "s/display_errors = .*/display_errors = On/" /etc/php/8.4/fpm/php.ini
+  sed -i "s/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/" /etc/php/8.4/fpm/php.ini
+  sed -i "s/memory_limit = .*/memory_limit = 512M/" /etc/php/8.4/fpm/php.ini
+  sed -i "s/upload_max_filesize = .*/upload_max_filesize = 100M/" /etc/php/8.4/fpm/php.ini
+  sed -i "s/post_max_size = .*/post_max_size = 100M/" /etc/php/8.4/fpm/php.ini
+  sed -i "s/;date.timezone.*/date.timezone = UTC/" /etc/php/8.4/fpm/php.ini
+
+  printf "[openssl]\n" | tee -a /etc/php/8.4/fpm/php.ini
+  printf "openssl.cainfo = /etc/ssl/certs/ca-certificates.crt\n" | tee -a /etc/php/8.4/fpm/php.ini
+  printf "[curl]\n" | tee -a /etc/php/8.4/fpm/php.ini
+  printf "curl.cainfo = /etc/ssl/certs/ca-certificates.crt\n" | tee -a /etc/php/8.4/fpm/php.ini
+
+  # Configure FPM
+  sed -i "s/user = www-data/user = vagrant/" /etc/php/8.4/fpm/pool.d/www.conf
+  sed -i "s/group = www-data/group = vagrant/" /etc/php/8.4/fpm/pool.d/www.conf
+  sed -i "s/listen\.owner.*/listen.owner = vagrant/" /etc/php/8.4/fpm/pool.d/www.conf
+  sed -i "s/listen\.group.*/listen.group = vagrant/" /etc/php/8.4/fpm/pool.d/www.conf
+  sed -i "s/;listen\.mode.*/listen.mode = 0666/" /etc/php/8.4/fpm/pool.d/www.conf
+
+  touch /home/vagrant/.homestead-features/php84
+
   # PHP 8.3
   apt-get install -y --allow-change-held-packages \
   php8.3 php8.3-bcmath php8.3-bz2 php8.3-cgi php8.3-cli php8.3-common php8.3-curl php8.3-dba php8.3-dev \
