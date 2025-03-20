b7fd9e74a5fe8e278bf06e7ceb0b4423b1c268e1
Joe Ferguson <joe@joeferguson.me>
2025-03-19 21:59:15 -0500

    Update arm.sh and add nvm to vagrant user

--- scripts/amd64.sh
+++ scripts/amd64.sh
@@ -684,6 +684,12 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.2/install.sh | bash
 # in lieu of restarting the shell
 \. "$HOME/.nvm/nvm.sh"
 
+sudo tee -a /home/vagrant/.bashrc <<EOL
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+EOL
+
 # Download and install Node.js:
 nvm install 22
 
